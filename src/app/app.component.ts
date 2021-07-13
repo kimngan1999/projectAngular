9674080f927159875921ab6c5bdb637b7f46ee96
root <root@vagrant.vm>
2021-07-13 02:26:34 +0000

    params

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   classNav = "nav";
   rearchClass = "search"
   title = 'projectAngular';
   navRightClass ="navRightClass"
   rowClass = "row";
+  
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    //console.log(123456);
+    
+    
+  }
 }
