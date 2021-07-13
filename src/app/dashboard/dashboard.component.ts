111ec9a9ac7f37369af873e2af0521a7185ab5c4
root <root@vagrant.vm>
2021-07-13 02:26:34 +0000

    params

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit,ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import {DomSanitizer,SafeResourceUrl,} from '@angular/platform-browser';
 
+declare  var jQuery:  any;
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
   @ViewChild('iframe') iframe!: ElementRef;
-  constructor() { }
+  // init_url:SafeResourceUrl = "http://172.29.65.197:3000/d/MFmXcoR7k/new-dashboard?orgId=1&from=now%2Fd&to=now%2Fd&var-show_value=data_out&var-value_above=10000&refresh=5s";
+  init_url:SafeResourceUrl = "http://172.29.65.197:3000/dashboard/script/scripted.js?orgId=1&refresh=5s&from=1609434000000&to=1625734491307";
+  constructor(public sanitizer:DomSanitizer) { }
 
+  
   ngOnInit(): void {
+    this.init_url = this.sanitizer.bypassSecurityTrustResourceUrl( "http://172.29.65.197:3000/dashboard/script/scripted.js?orgId=1&refresh=5s&from=now%2Fd&to=now%2Fd&kiosk=tv");
+    //console.log(this.init_url);
+    
+    
+    
+    
   }
 
   OnClickIframe(): void {
@@ -18,6 +29,65 @@ export class DashboardComponent implements OnInit {
     console.log("passsssssss")
     console.log("nativeEl",nativeEl)
     console.log("nativeEl1",nativeEl1)
+  }
+
+  ngAfterViewInit(): void{
+
+    
+    var mainURL = window.location.href;
+
+    let iframe  = document.getElementById("dashboard") ;
+
+
+
+
+
+
+    (function ($) {
+
+      $(document).ready(function(){
+        //console.log("Hello from jQuery!");
+
+
+
+        let locationValue = new URL("http://172.29.65.197:3000/dashboard/script/scripted.js?orgId=1&refresh=5s&from=now%2Fd&to=now%2Fd&kiosk=tv");
+        let winURL = new URL(mainURL);
+        if(winURL.search == ""){
+          window.location.href = mainURL + locationValue.search;
+          //console.log('null');
+          
+        }
+        else{
+          //console.log(winURL.search);
+          let new_url = "http://172.29.65.197:3000/dashboard/script/scripted.js" + winURL.search;
+          $("#dashboard").attr('src',new_url);
+          //console.log($("#dashboard")[0].contentWindow.document.querySelector("div[id='reactRoot']"));
+          
+        }
+
+        window.addEventListener("message", function (event) {
+          //console.log(event.data);
+          let locationValue = new URL(event.data);
+          //console.log(winURL.origin + winURL.pathname);
+          if (window.location.href == "http://172.29.65.197:3000/dashboard/script/scripted.js?orgId=1&refresh=5s&from=now%2Fd&to=now%2Fd&kiosk=tv") {
+            
+          }
+          if(window.location.href != (winURL.origin + winURL.pathname + locationValue.search)){
+            console.log(event.data);
+
+            let nextState = "change URL";
+            let nextTitle = "change URL";
+            let nextURL = winURL.origin + winURL.pathname + locationValue.search;
+            window.history.replaceState(nextState, nextTitle, nextURL);
+          }
+          
+
+        }, false);
+      //console.log(locationValue.search);
+
+        
+      });
+    })(jQuery);
   }
 
 }
