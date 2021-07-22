f1e525de5f08597db69b3e38a9bc72632e4c443a
khanh <khanh451754@gmail.com>
2021-07-22 11:06:03 +0700

    undo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Router } from "@angular/router";
 import { element } from 'protractor';
 import { AuthService } from '../Services/auth.service';
-import { HttpClient } from '@angular/common/http';
 // import { UserService } from '../Services/user.service';
 
 @Component({
@@ -33,28 +32,13 @@ export class HomeComponent implements OnInit {
     
 
   
-    constructor(private router: Router, private http: HttpClient) { }
+    constructor(private router: Router) { }
   
     ngOnInit(): void {
       this.innerWidth = window.innerWidth;
       this.innerHeight = window.innerHeight; 
     }
 
-    OnLoginGrafana(){
-      const headers = {  'Access-Control-Allow-Credentials': 'true', 
-      'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, HEAD, OPTIONS, PATCH, PROPFIND, PROPPATCH, MKCOL, COPY, MOVE, LOCK', 
-      'Access-Control-Allow-Origin': 'http://172.29.65.197',
-      'Access-Control-Allow-Headers': 'Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range',
-      'accept': 'application/json, text/plain, */*',
-      'Accept-Language': 'en-US,en;q=0.9',
-      'content-type': 'application/json',
-      'x-grafana-org-id': '1', };
-      const body = {"user": "user_editor", "password": "123456"}
-      this.http.post<any>('http://172.29.65.197/login', body, {headers}).subscribe(data => {
-        console.log(data);
-    });
-    }
-    
     
  
 }
